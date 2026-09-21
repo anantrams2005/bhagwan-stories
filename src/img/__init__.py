3185c2cf8c6cb786cbefc3967c6708db55e06ae3
anantrams2005 <anantrams2005@gmail.com>
2026-09-22 00:41:31 +1000

    refactor: resolve reusable assets before scene generation

--- src/img/__init__.py
+++ src/img/__init__.py
@@ -1,6 +1,16 @@
-"""Image generation stages: character references first, then scene images."""
+"""Image generation stages and reusable asset resolution."""
+
+from .assets import AssetLibrary, AssetStage
 from .characters import CharacterImageStage
 from .scenes import SceneImageStage
+from .supporting import SupportingAssetStage
 from .comfyui import ComfyUIImageGenerator
 
-__all__ = ["CharacterImageStage", "SceneImageStage", "ComfyUIImageGenerator"]
+__all__ = [
+    "AssetLibrary",
+    "AssetStage",
+    "CharacterImageStage",
+    "SupportingAssetStage",
+    "SceneImageStage",
+    "ComfyUIImageGenerator",
+]
