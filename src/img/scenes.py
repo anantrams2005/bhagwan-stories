b5d2e068ad42ff7795c58bdd619f883d217286d1
anantrams2005 <anantrams2005@gmail.com>
2026-09-22 00:42:03 +1000

    refactor: send asset refs to scene image adapter

--- src/img/scenes.py
+++ src/img/scenes.py
@@ -5,7 +5,7 @@ from typing import Any
 
 
 class SceneImageStage:
-    """Compose each shot from the story prompt plus resolved visual assets."""
+    """Compose each shot from the prompt plus reusable visual asset references."""
 
     def __init__(self, generator: Any) -> None:
         self.generator = generator
@@ -24,27 +24,21 @@ class SceneImageStage:
             for shot in scene["shots"]:
                 shot_dir = movie_dir / "scenes" / scene["id"] / shot["id"]
                 asset_ids = shot.get("assets", shot.get("characters", []))
-                refs = [str(asset_refs[a]) for a in asset_ids if a in asset_refs]
-
-                prompt = shot["image_prompt"]
-                if refs:
-                    prompt = (
-                        f"{prompt}\n\nVISUAL REFERENCE ASSETS:\n"
-                        + "\n".join(refs)
-                    )
+                refs = [asset_refs[a] for a in asset_ids if a in asset_refs]
 
                 image = self.generator.generate(
-                    prompt,
+                    shot["image_prompt"],
                     shot.get("negative_prompt", ""),
                     shot_dir,
                     "source.png",
+                    reference_images=refs,
                 )
                 records.append({
                     "scene_id": scene["id"],
                     "shot_id": shot["id"],
                     "duration": shot["duration"],
                     "image": str(image),
-                    "asset_refs": refs,
+                    "asset_refs": [str(p) for p in refs],
                     "video": None,
                 })
         return records
